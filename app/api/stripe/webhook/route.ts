59c07663260a75601dbc0d296361a00af87b784f
kevin.lim <kevin.lim@blockchainsg.com>
2024-03-10 16:55:19 +0800

    fix stripe webhook handle function

--- app/api/stripe/webhook/route.ts
+++ app/api/stripe/webhook/route.ts
@@ -6,26 +6,27 @@ import { updateOrderById } from '@/app/actions/orders';
 import { redirect } from 'next/navigation';
 const stripe = new Stripe(process.env.NEXT_STRIPE_SECRET_KEY!);
 
-export async function POST(req: NextRequest, res: NextResponse) {
+export async function POST(req: NextRequest) {
   const payload = await req.text();
-  const sig = req.headers.get('stripe-signature');
+  const res = JSON.parse(payload);
+
+  const sig = req.headers.get('Stripe-Signature');
 
   try {
     let event = stripe.webhooks.constructEvent(payload, sig!, process.env.NEXT_STRIPE_WEBHOOK_SECRET!);
     console.log('Event', event.type);
-    // charge.succeeded
-    // payment_intent.succeeded
-    // payment_intent.created
 
     // Handle the event
     switch (event.type) {
       case 'checkout.session.completed':
-        const { userId, orderId }: any = event.data.object.metadata;
-        const result = await updateOrderById(orderId, {
-          orderStatus: 'completed',
-        });
-        console.log(result);
-        redirect(`${process.env.NEXT_UI_URL}/profile`);
+        const { userId, orderId }: any = res.data.object.metadata;
+        if (userId && orderId) {
+          const result = await updateOrderById(orderId, {
+            orderStatus: 'completed',
+          });
+          console.log(result);
+          redirect(`${process.env.NEXT_UI_URL}/profile`);
+        }
         break;
 
       default:
@@ -33,6 +34,6 @@ export async function POST(req: NextRequest, res: NextResponse) {
 
     return NextResponse.json({ message: event.type }, { status: 200 });
   } catch (error: any) {
-    return NextResponse.json({ message: `Webhook Error: ${error.message}` }, { status: 400 });
+    return NextResponse.json({ message: `Webhook Error: ${error.message}` }, { status: 200 });
   }
 }
